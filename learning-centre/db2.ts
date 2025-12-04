6da8f25b1c96ffa3ada7961756cf6995064e4a70
tommy <lamchiho27@yahoo.com.hk>
2025-12-04 22:16:52 +0800

    Delete the extra database.sqlite outside, and set the db2.ts path to point to database.sqlite

--- learning-centre/db2.ts
+++ learning-centre/db2.ts
@@ -1,6 +1,7 @@
 import { createDB } from "better-sqlite3-schema";
 import path from "path";
 
+// 運行時 __dirname = dist/，所以用 .. 返回 learning-centre/
 export let db = createDB({
   file: path.join(__dirname, "..", "database.sqlite"),
 });
